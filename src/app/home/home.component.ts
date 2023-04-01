85e6559449644700e3bbb1898298b954a2eb3563
ProgramerB <Bhavesh2000@outlook.com>
2023-04-01 12:38:23 +0530

    added server request

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { recipe } from '../recipe-page/recipe';
 import { RecipeService } from '../recipe-page/recipe.service';
 
@@ -7,10 +8,13 @@ import { RecipeService } from '../recipe-page/recipe.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  imageWidth:number = 100;
-  imageMargin:number = 2;
-  filteredRecipes:recipe[]=[];
+export class HomeComponent implements OnInit , OnDestroy{
+  imageWidth: number = 100;
+  imageMargin: number = 2;
+  filteredRecipes: recipe[]=[];
+  errorMessage: string = '';
+  sub!: Subscription;
+
   private _listFilter:string = '';
   get listFilter(): string{
     return this._listFilter;
@@ -22,9 +26,19 @@ export class HomeComponent implements OnInit {
   recipes:recipe[] = []
 
   ngOnInit(): void {
-      console.log("ngOnInit");
-      this.recipes = this.recipeService.getRecipes();
-      this.filteredRecipes = this.recipes;
+    this.sub = this.recipeService.getRecipes().subscribe({
+      next : recipes => {
+        this.recipes = recipes.meals;
+        this.filteredRecipes = this.recipes;
+      },
+      error: err => this.errorMessage = err
+    });
+      // this.recipes = this.recipeService.getRecipes();
+      // this.filteredRecipes = this.recipes;
+  }
+
+  ngOnDestroy(): void {
+      this.sub.unsubscribe();
   }
 
   constructor(private recipeService: RecipeService){}
--- src/app/recipe-page/recipe.service.ts
+++ src/app/recipe-page/recipe.service.ts
@@ -1,26 +1,51 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable, catchError, tap, throwError } from "rxjs";
+import { meals } from "./meal";
 import { recipe } from "./recipe";
 
 @Injectable({
     providedIn:'root'
 })
 export class RecipeService{
-    getRecipes(): recipe[]{
-        return [
-            {
-                "idMeal": 52879,
-                "strMeal": "Chicken Parmentier",
-                "strCategory": "Chicken",
-                "strArea": "French",
-                "strMealThumb": "https://www.themealdb.com/images/media/meals/uwvxpv1511557015.jpg",
-              },
-              {
-                "idMeal": 52835,
-                "strMeal": "Fettucine alfredo",
-                "strCategory": "Pasta",
-                "strArea": "Italian",
-                "strMealThumb": "https://www.themealdb.com/images/media/meals/uquqtu1511178042.jpg",
-              }
-        ]
+    private url = 'http://localhost:8080/api/mealDB/random';
+    constructor(private http:HttpClient){}
+
+    getRecipes(): Observable<meals>{
+    //     return [
+    //         {
+    //             "idMeal": 52879,
+    //             "strMeal": "Chicken Parmentier",
+    //             "strCategory": "Chicken",
+    //             "strArea": "French",
+    //             "strMealThumb": "https://www.themealdb.com/images/media/meals/uwvxpv1511557015.jpg",
+    //           },
+    //           {
+    //             "idMeal": 52835,
+    //             "strMeal": "Fettucine alfredo",
+    //             "strCategory": "Pasta",
+    //             "strArea": "Italian",
+    //             "strMealThumb": "https://www.themealdb.com/images/media/meals/uquqtu1511178042.jpg",
+    //           }
+    //     ]
+    // }
+        return this.http.get<meals>(this.url).pipe(
+            tap( data => console.log('ALL',JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError( err:HttpErrorResponse ){
+        let errorMessage = '';
+        if(err.error instanceof ErrorEvent){
+            //client side error
+            errorMessage = `An error occured: ${err.message}`;
+        }
+        else{
+            //server side error
+            errorMessage = `Server return code: ${err.status}, error message is ${err.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(()=>errorMessage);
     }
 }
