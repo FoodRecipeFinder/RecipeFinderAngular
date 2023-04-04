38fe60b6eb0723ff43f97d91e40a0d8a7d4e70a5
harshalaardekar101@gmail.com <harshalaardekar101@gmail.com>
2023-04-05 00:24:48 +0530

    save and remove recipes code

--- src/app/recipe-page/recipe-page.component.ts
+++ src/app/recipe-page/recipe-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, InjectFlags, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { recipe } from './recipe';
 import { RecipeService } from './recipe.service';
+import { LoginService } from '../login-service.service';
 
 @Component({
   selector: 'app-recipe-page',
@@ -13,7 +14,13 @@ export class RecipePageComponent implements OnInit {
   recipe: recipe | undefined;
   sub!: Subscription;
   errorMessage = '';
-  constructor(private route: ActivatedRoute, private router: Router, private recipeService: RecipeService){}
+  mealId : number =0;
+  userId : number = 1008;
+  value : boolean | undefined;
+  btnText : String='';
+
+  saveButton:boolean | undefined;
+  constructor(private route: ActivatedRoute, private router: Router, private recipeService: RecipeService,private service : LoginService){}
   
   ngOnInit(): void{
     const id = Number(this.route.snapshot.paramMap.get('id'));  
@@ -21,12 +28,49 @@ export class RecipePageComponent implements OnInit {
     this.sub = this.recipeService.getRecipeById(id).subscribe({
       next : recipes => {
         this.recipe = recipes.meals[0];
+        this.checkIfSaved();
       },
       error: err => this.errorMessage = err
+
     });
+
+   
   }
 
-  onBack(): void{
-    this.router.navigate(['/home']);
+  
+  checkIfSaved(){
+     this.service.checkIfSaved(this.userId,this.recipe?.idMeal!).subscribe(
+      res => {
+        console.log(this.mealId," ",res," ",this.recipe?.idMeal);
+        this.saveButton=res;
+        if (this.saveButton) {
+          document.getElementById("saveButton")?.setAttribute('disabled','');
+          this.btnText="Already Saved"
+        } else {
+          this.btnText="Save for later"
+        }
+      }
+    )
   }
+
+  saveRecipe():void{
+    
+    this.service.saveRecipe(this.userId,this.recipe?.idMeal!).subscribe(
+      res=>{
+        this.value=res;
+        console.log(this.mealId," ",res," ",this.recipe?.idMeal);
+        if(this.value){
+          alert("Recipe saved");
+          window.location.reload();
+        }
+        else{
+          alert('Something went wrong!!! Try again');
+        }
+      }
+    )
+  }
+
+  // onBack(): void{
+  //   this.router.navigate(['/home']);
+  // }
 }
