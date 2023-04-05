c6d07b405c299d54a08fee10fdc5b2ca9bfba59e
ProgramerB <Bhavesh2000@outlook.com>
2023-04-05 11:41:38 +0530

    added environment variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    prod: true,
+    baseUrl: 'REPLACE WITH BASE URL HERE //DOESNOT WORK CURRENTLY',
+    envVar: {
+        springUrl:'REPLACE WITH SPRING URL HERE',
+        LOG_LEVEL: 'debug',
+        version: 0
+    }
+};
+// This is just a template, create environment.development.ts for actuall use case 
