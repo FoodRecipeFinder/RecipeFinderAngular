0edce3c461ec44eba8c9333549bf755f4ef28c35
ProgramerB <Bhavesh2000@outlook.com>
2023-04-14 22:05:50 +0530

    changed spinners from blocks to food emojis

--- src/app/shared/spinner/spinner-black.component.ts
+++ src/app/shared/spinner/spinner-black.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spinner-black',
   templateUrl: './spinner-black.component.html',
   styleUrls: ['./spinner-black.component.css']
 })
-export class SpinnerBlackComponent {
-
+export class SpinnerBlackComponent implements OnInit { 
+  foods = ["🍕","🥤","🍺","🧃","🍰","☕","🥩","🍗","🍖","🍩","🥫"]
+  food1!: string;
+  food2!: string;
+  ngOnInit(): void {
+    this.food1 = this.foods[Math.floor(Math.random() * this.foods.length)]
+    this.food2 = this.foods[Math.floor(Math.random() * this.foods.length)]
+  }
 }
