797ae15d346f7d2157f3a531d662adc18d78201a
ProgramerB <Bhavesh2000@outlook.com>
2023-05-04 09:32:41 +0530

    moved routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RecipePageGuard } from './recipe-page/recipe-page.guard';
+import { RecipePageComponent } from './recipe-page/recipe-page.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { SavedRecipesComponent } from './saved-recipes/saved-recipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home',component: HomeComponent},
+  {path: 'recipe/:id',canActivate: [RecipePageGuard],component: RecipePageComponent},
+  {path: 'login',component:UserLoginComponent},
+  {path: 'savedRecipes',component:SavedRecipesComponent},
+  {path: '**', redirectTo:'/home',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
