25405ad06348fb084b8958b2d74b0a9174249f03
ProgramerB <Bhavesh2000@outlook.com>
2023-11-13 21:00:23 +0530

    removed port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     prod: true,
     baseUrl: 'http://localhost:80',
     envVar: {
-        springUrl:'https://recipe-finder-spring.onrender.com:8080',
+        springUrl:'https://recipe-finder-angular.onrender.com',
         LOG_LEVEL: 'debug',
         version: 0
     }
